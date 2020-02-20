1d0c5c36e5e7aeec26f220090d6899b69119444b
NexGenUA <mr.rinus@gmail.com>
2020-02-20 22:03:26 +0200

    init: task complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return Math.max(...array) || 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.reduce((acc, cur) => acc + cur) / array.length;
 }
